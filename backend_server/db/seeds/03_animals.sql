263836bc1fc5c3a22341fbc073c6be2dcd52d109
Neavy18 <genevieve.gt18@gmail.com>
2022-02-17 10:09:50 -0500

    Add specie column to table

--- backend_server/db/seeds/03_animals.sql
+++ backend_server/db/seeds/03_animals.sql
@@ -1,2 +1,2 @@
-INSERT INTO animals(refuge_id, name, age, breed, description, image) VALUES 
-(1, 'Baloo', 2, 'Malamute', 'Good boy looking for his furever home', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.akc.org%2Fdog-breeds%2Falaskan-malamute%2F&psig=AOvVaw0oah6AQgzOgh0ddjIxauvw&ust=1645147335855000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMjoy8fJhfYCFQAAAAAdAAAAABAD');
+INSERT INTO animals(refuge_id, specie, name, age, breed, description, image) VALUES 
+(1, 'dog',io 'Baloo', 2, 'Malamute', 'Good boy looking for his furever home', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.akc.org%2Fdog-breeds%2Falaskan-malamute%2F&psig=AOvVaw0oah6AQgzOgh0ddjIxauvw&ust=1645147335855000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMjoy8fJhfYCFQAAAAAdAAAAABAD');

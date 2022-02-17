96e8d448107ec598c81d932e342a54756499fed9
Neavy18 <genevieve.gt18@gmail.com>
2022-02-17 11:12:39 -0500

    Add 4 react routes

--- frontend_app/src/App.js
+++ frontend_app/src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+//import { Route } from 'react-router';
 import Layout from './components/Layout';
 import Home from './components/Home';
-
+import Login from './components/Login';
+import Register from './components/Register';
+import Favorites from './components/Favorites';
 function App() {
   return (
-    <Layout>
-     <Home></Home>
-    </Layout>
+    <Router>
+      <Routes>
+        <Route path ="/" element={<Home />} />
+        <Route path = "/login" element={<Login />}/>
+        <Route path = "/register" element={<Register />}/>
+        <Route path = "/favorites" element={<Favorites />} />
+      </Routes>
+    </Router>
+  
   );
 }
-
+ 
 export default App;
---
+++ frontend_app/src/components/Login/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className='Login'>
+      <h2>LOGIN</h2>
+      <button>Login</button>
+    </div>
+  )
+}
+
+// everything related to Header goes here :  onClick js, html, icons etc
+// add logic to display icons at top when user is logged in, hide if not
+
+export default Login;
